4923a99d0a311d1316e7b139fe66273a9920f6d7
Anastasia <55848388+Estasie@users.noreply.github.com>
2023-07-14 17:03:44 +0400

    feat: add imports of actions toolbar (#72)
    * feat: add imports of actions toolbar
    
    * fix: import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export * from './DashKit';
+export {ActionsToolbar} from './ActionsToolbar/ActionsToolbar';
+
 export type {OverlayControlItem} from './OverlayControls/OverlayControls';
+export type {ActionsToolbarButton} from './ActionsToolbar/ActionsToolbar';
