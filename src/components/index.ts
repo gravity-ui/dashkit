bba00241595f0ce12ee01ac166124c1c4ea90484
Anastasia Beria <estasieberia@yandex-team.ru>
2023-07-14 16:59:52 +0400

    fix: import

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export * from './DashKit';
 export {ActionsToolbar} from './ActionsToolbar/ActionsToolbar';
 
 export type {OverlayControlItem} from './OverlayControls/OverlayControls';
-export type {ActionsToolbarProps} from './ActionsToolbar/ActionsToolbar';
+export type {ActionsToolbarButton} from './ActionsToolbar/ActionsToolbar';
