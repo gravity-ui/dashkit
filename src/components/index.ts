0e844c71c0bcfcdff8650147e7f18fa469dfa5ae
Evgeny Alaev <alaev89@yandex-team.ru>
2023-07-17 14:18:04 +0300

    Revert "feat: add imports of actions toolbar (#72)"
    This reverts commit 4923a99d0a311d1316e7b139fe66273a9920f6d7.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,2 @@
 export * from './DashKit';
-export {ActionsToolbar} from './ActionsToolbar/ActionsToolbar';
-
 export type {OverlayControlItem} from './OverlayControls/OverlayControls';
-export type {ActionsToolbarButton} from './ActionsToolbar/ActionsToolbar';
