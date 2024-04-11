d052702b6e4b736fe2ac67c8488f3ec1951096fb
Elena Martynova <marginy@yandex-team.ru>
2024-04-11 18:21:48 +0300

    feat: add attribute to plugin root element (#135)

--- src/plugins/Title/Title.tsx
+++ src/plugins/Title/Title.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {Plugin, PluginWidgetProps} from '../../typings';
 import {cn} from '../../utils/cn';
+import {PLUGIN_ROOT_ATTR_NAME} from '../constants';
 
 import './Title.scss';
 
@@ -22,7 +23,7 @@ export class PluginTitle extends React.Component<PluginTitleProps> {
         const size = data.size ? data.size : false;
         const id = data.showInTOC && text ? encodeURIComponent(text) : undefined;
         return (
-            <div id={id} className={b({size})}>
+            <div id={id} className={b({size})} {...{[PLUGIN_ROOT_ATTR_NAME]: 'title'}}>
                 {text}
             </div>
         );
