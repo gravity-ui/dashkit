0286e01e02c444480e347cb0af8c1f45f7202632
Serge Pavlyuk <sergeypavlyuk@yandex-team.ru>
2024-07-22 11:15:55 +0300

    fix: removed undefined parent (#165)

--- src/components/GridLayout/GridLayout.js
+++ src/components/GridLayout/GridLayout.js
@@ -171,7 +171,13 @@ export default class GridLayout extends React.PureComponent {
 
         const newItemsLayoutById = newLayout.reduce((memo, item) => {
             const parent = itemsByGroup[item.i].parent;
-            memo[item.i] = {...item, parent};
+
+            memo[item.i] = {...item};
+
+            if (parent) {
+                memo[item.i].parent = parent;
+            }
+
             return memo;
         }, {});
 
