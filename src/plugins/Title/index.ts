7e6e921392341eb58673cf09be8852e2bd461395
Daria Larionova <darialari@yandex-team.ru>
2025-01-14 16:10:08 +0300

    fix(Title): export all constants from Title

--- src/plugins/Title/index.ts
+++ src/plugins/Title/index.ts
@@ -1,4 +1,4 @@
 export * from './Title';
 export {default as pluginTitle} from './Title';
 export {PluginTitleSize, TitleFontParams} from './types';
-export {TITLE_DEFAULT_SIZES} from './constants';
+export * from './constants';
