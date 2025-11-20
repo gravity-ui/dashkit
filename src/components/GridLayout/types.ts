8cda78edb8a853f651856aa1c18e9182bd7075d3
ykornilov <kornilovyv81@gmail.com>
2025-11-20 10:52:30 +0300

    fix!: move GridLayout from JS to TS (#277)
    * fix!: move GridLayout from JS to TS
    
    * chore: update ci workflow
    
    ---------
    
    Co-authored-by: Yury Kornilov <kornilovyv@yandex-team.ru>

---
+++ src/components/GridLayout/types.ts
@@ -0,0 +1,77 @@
+import type {ReactGridLayoutProps} from 'src/typings';
+
+import type {ConfigItem, ConfigLayout, ItemDragProps} from '../../shared';
+
+export type GridLayoutProps = {
+    ref?: React.ForwardedRef<unknown>;
+};
+
+export type GridLayoutState = {
+    isDragging: boolean;
+    isDraggedOut: boolean;
+    isPageHidden: boolean;
+    currentDraggingElement: CurrentDraggingElement | null;
+    draggedOverGroup: string | null;
+};
+
+export type CurrentDraggingElement = {
+    group: string;
+    layoutItem: ConfigLayout;
+    item: ConfigItem | ItemDragProps;
+    cursorPosition: {
+        offsetX: number;
+        offsetY: number;
+    };
+};
+
+export type ReloadItemsOptions = {
+    targetIds?: string[];
+    force?: boolean;
+};
+
+export type ManipulationCallbackArgs = {
+    group: string;
+    layout: ConfigLayout[];
+    oldItem: ConfigLayout;
+    newItem: ConfigLayout;
+    placeholder: ConfigLayout;
+    e: MouseEvent;
+    element: HTMLElement;
+};
+
+export type MemoGroupLayout = {
+    key: string;
+    layout: ConfigLayout[];
+};
+
+type GroupCallback = (
+    layout: ConfigLayout[],
+    layoutItem: ConfigLayout,
+    newItem: ConfigLayout,
+    placeholder: ConfigLayout,
+    event: MouseEvent,
+    element: HTMLElement,
+) => void;
+
+export type GroupCallbacks = {
+    onDragStart: GroupCallback;
+    onDrag: GroupCallback;
+    onDragStop: GroupCallback;
+    onResizeStart: GroupCallback;
+    onResize: GroupCallback;
+    onResizeStop: GroupCallback;
+    onDrop: (layout: ConfigLayout[], item: ConfigLayout | undefined, e: MouseEvent) => void | false;
+    onDropDragOver: (e: DragEvent | MouseEvent) => void | boolean;
+    onDragTargetRestore: (group?: string) => void;
+};
+
+export type AdjustWidgetLayoutParams = {
+    widgetId: string;
+    needSetDefault?: boolean;
+    adjustedWidgetLayout?: ConfigLayout;
+};
+
+export type LayoutAndPropsByGroup = {
+    properties: Partial<ReactGridLayoutProps>;
+    layout: ConfigLayout[];
+};
