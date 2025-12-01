e052dedb682dcc19c04324ab880ecba5433fc0a8
Yury Kornilov <kornilovyv@yandex-team.ru>
2025-12-01 14:41:38 +0300

    chore: restore js

---
+++ src/hocs/prepareItem.js
@@ -0,0 +1,111 @@
+import React from 'react';
+
+import isEqual from 'lodash/isEqual';
+import PropTypes from 'prop-types';
+
+import {DashKitContext} from '../context';
+
+export function prepareItem(Component) {
+    return class PrepareItem extends React.Component {
+        static propTypes = {
+            gridLayout: PropTypes.object,
+            adjustWidgetLayout: PropTypes.func.isRequired,
+            layout: PropTypes.array,
+            id: PropTypes.string,
+            item: PropTypes.object,
+            shouldItemUpdate: PropTypes.bool,
+            width: PropTypes.number,
+            height: PropTypes.number,
+            transform: PropTypes.string,
+            isPlaceholder: PropTypes.bool,
+
+            onItemRender: PropTypes.func,
+            onItemMountChange: PropTypes.func,
+
+            forwardedPluginRef: PropTypes.any,
+        };
+
+        shouldComponentUpdate(nextProps) {
+            const {width, height, transform} = this.props;
+            const {width: widthNext, height: heightNext, transform: transformNext} = nextProps;
+            if (
+                !nextProps.shouldItemUpdate &&
+                width === widthNext &&
+                height === heightNext &&
+                transform === transformNext
+            ) {
+                return false;
+            }
+            return true;
+        }
+
+        static contextType = DashKitContext;
+
+        _onStateAndParamsChange = (stateAndParams, options) => {
+            this.context.onItemStateAndParamsChange(this.props.id, stateAndParams, options);
+        };
+
+        _currentRenderProps = {};
+        getRenderProps = () => {
+            const {id, width, height, item, adjustWidgetLayout, layout, isPlaceholder, gridLayout} =
+                this.props;
+            const {itemsState, itemsParams, registerManager, settings, context, editMode} =
+                this.context;
+            const {data, defaults, namespace} = item;
+
+            const rendererProps = {
+                data,
+                editMode,
+                params: itemsParams[id],
+                state: itemsState[id],
+                onStateAndParamsChange: this._onStateAndParamsChange,
+                width,
+                height,
+                id,
+                defaults,
+                namespace,
+                settings,
+                context,
+                layout,
+                gridLayout: gridLayout || registerManager.gridLayout,
+                adjustWidgetLayout,
+                isPlaceholder,
+            };
+
+            const changedProp = Object.entries(rendererProps).find(([key, value]) => {
+                // Checking gridLayoout deep as groups gridProperties method has tendancy to creat new objects
+                if (key === 'gridLayout') {
+                    return !isEqual(this._currentRenderProps[key], value);
+                }
+
+                return this._currentRenderProps[key] !== value;
+            });
+
+            if (changedProp) {
+                this._currentRenderProps = rendererProps;
+            }
+
+            return this._currentRenderProps;
+        };
+
+        render() {
+            const {item, isPlaceholder, forwardedPluginRef, onItemMountChange, onItemRender} =
+                this.props;
+            const {registerManager} = this.context;
+            const {type} = item;
+
+            return (
+                <Component
+                    forwardedPluginRef={forwardedPluginRef}
+                    rendererProps={this.getRenderProps()}
+                    registerManager={registerManager}
+                    type={type}
+                    isPlaceholder={isPlaceholder}
+                    onItemMountChange={onItemMountChange}
+                    onItemRender={onItemRender}
+                    item={item}
+                />
+            );
+        }
+    };
+}
